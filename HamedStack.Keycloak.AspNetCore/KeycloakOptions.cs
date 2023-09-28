88122641a05b1b1398a034d7988bb3d5c3d38880
hamedfathi <hamedfathi@outlook.com>
2023-09-28 23:30:57 +0330

    feat(project): source codes added.

---
+++ HamedStack.Keycloak.AspNetCore/KeycloakOptions.cs
@@ -0,0 +1,144 @@
+﻿// ReSharper disable IdentifierTypo
+// ReSharper disable UnusedMember.Global
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+
+namespace HamedStack.Keycloak.AspNetCore;
+
+/// <summary>
+/// Provides configuration options for integrating with Keycloak for authentication and authorization.
+/// </summary>
+/// <remarks>
+/// This configuration is essential for setting up OpenID Connect with Keycloak.
+/// </remarks>
+/// <example>
+/// Here's how you can use the <see cref="KeycloakOptions"/>:
+/// <code>
+/// var options = new KeycloakOptions
+/// {
+///     Authority = "https://keycloak.example.com/auth/realms/myrealm",
+///     ClientId = "my-client-id",
+///     ClientSecret = "my-client-secret"
+/// };
+/// </code>
+/// </example>
+public class KeycloakOptions
+{
+    /// <summary>
+    /// Gets or sets the Keycloak authority URL.
+    /// </summary>
+    /// <remarks>
+    /// This is the URL of the Keycloak server's realm where your client is defined.
+    /// Typically, it's in the format "https://[keycloak-domain]/auth/realms/[realm-name]".
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// options.Authority = "https://keycloak.example.com/auth/realms/myrealm";
+    /// </code>
+    /// </example>
+    public string Authority { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the client ID used for authentication with Keycloak.
+    /// </summary>
+    /// <remarks>
+    /// This is the client identifier registered in your Keycloak realm.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// options.ClientId = "my-client-id";
+    /// </code>
+    /// </example>
+    public string ClientId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the client secret used for authentication with Keycloak.
+    /// </summary>
+    /// <remarks>
+    /// This is the secret associated with your client in Keycloak. It's used in confidential client types.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// options.ClientSecret = "my-client-secret";
+    /// </code>
+    /// </example>
+    public string ClientSecret { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets whether HTTPS is mandatory for metadata retrieval from Keycloak.
+    /// </summary>
+    /// <remarks>
+    /// It's recommended to always use HTTPS in production setups for security.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// options.RequireHttpsMetadata = true;
+    /// </code>
+    /// </example>
+    public bool RequireHttpsMetadata { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the default redirect URI after authentication.
+    /// </summary>
+    /// <remarks>
+    /// This is where Keycloak will redirect the user after a successful authentication.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// options.DefaultRedirectUri = "/home";
+    /// </code>
+    /// </example>
+    public string DefaultRedirectUri { get; set; } = "/";
+
+    /// <summary>
+    /// Gets or sets whether the token issuer should be validated.
+    /// </summary>
+    /// <remarks>
+    /// It's a best practice to validate the issuer to ensure the token was issued by a trusted Identity provider like Keycloak.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// options.ValidateIssuer = true;
+    /// </code>
+    /// </example>
+    public bool ValidateIssuer { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the valid issuer for token validation.
+    /// </summary>
+    /// <remarks>
+    /// This should match the issuer claim in the token from Keycloak.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// options.ValidIssuer = "https://keycloak.example.com/auth/realms/myrealm";
+    /// </code>
+    /// </example>
+    public string? ValidIssuer { get; set; }
+
+    /// <summary>
+    /// Gets or sets the valid audiences for token validation.
+    /// </summary>
+    /// <remarks>
+    /// This ensures the token was intended for the correct audience, often the application itself.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// options.ValidAudiences = new[] { "my-client-id" };
+    /// </code>
+    /// </example>
+    public IEnumerable<string>? ValidAudiences { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the token lifetime should be validated.
+    /// </summary>
+    /// <remarks>
+    /// Validating the token's lifetime ensures that it hasn't expired and is still valid for use.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// options.ValidateLifetime = true;
+    /// </code>
+    /// </example>
+    public bool ValidateLifetime { get; set; } = true;
+}
---
+++ HamedStack.Keycloak.AspNetCore/KeycloakServiceCollection.cs
@@ -0,0 +1,118 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable InconsistentNaming
+// ReSharper disable IdentifierTypo
+// ReSharper disable UnusedMember.Global
+
+using IdentityModel;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HamedStack.Keycloak.AspNetCore;
+
+/// <summary>
+/// Provides extension methods for integrating Keycloak authentication and authorization with the provided services.
+/// </summary>
+/// <remarks>
+/// This service collection simplifies the configuration for using Keycloak as an OpenID Connect Identity provider.
+/// </remarks>
+public static class KeycloakServiceCollection
+{
+    private const string CODE_RESPONSE_TYPE = "code";
+
+    /// <summary>
+    /// Adds and configures Keycloak authentication using OpenID Connect and JWT Bearer token validation.
+    /// </summary>
+    /// <param name="services">The service collection to add the authentication services to.</param>
+    /// <param name="keycloakOptions">Configuration options for the Keycloak integration.</param>
+    /// <param name="httpClientFactory">Factory to create instances of <see cref="System.Net.Http.HttpClient"/>.</param>
+    /// <returns>The same service collection so that multiple calls can be chained.</returns>
+    /// <remarks>
+    /// This method configures the OpenID Connect handler for the application to integrate with Keycloak.
+    /// It handles the OpenID Connect Code flow, token validation, and sets up JWT Bearer token authentication.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// var keycloakOptions = new KeycloakOptions
+    /// {
+    ///     Authority = "https://keycloak.example.com/auth/realms/myrealm",
+    ///     ClientId = "my-client-id",
+    ///     ClientSecret = "my-client-secret"
+    /// };
+    ///
+    /// services.AddKeycloakAuthentication(keycloakOptions, httpClientFactory);
+    /// </code>
+    /// </example>
+    public static IServiceCollection AddKeycloakAuthentication(this IServiceCollection services, KeycloakOptions keycloakOptions, IHttpClientFactory httpClientFactory)
+    {
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddOpenIdConnect(options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.Authority = keycloakOptions.Authority;
+                options.RequireHttpsMetadata = keycloakOptions.RequireHttpsMetadata;
+                options.ClientId = keycloakOptions.ClientId;
+                options.ResponseType = CODE_RESPONSE_TYPE;
+                options.SaveTokens = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.TokenValidationParameters.ValidIssuer = keycloakOptions.ValidIssuer;
+                options.TokenValidationParameters.ValidateIssuer = keycloakOptions.ValidateIssuer;
+                options.TokenValidationParameters.ValidAudiences = keycloakOptions.ValidAudiences;
+                options.TokenValidationParameters.ValidateLifetime = keycloakOptions.ValidateLifetime;
+                options.Events = new OpenIdConnectEvents
+                {
+                    OnAuthorizationCodeReceived = async context =>
+                    {
+                        var httpClient = httpClientFactory.CreateClient();
+                        var discoveryResponse = await httpClient.GetDiscoveryDocumentAsync(context.Options.Authority);
+
+                        if (discoveryResponse.IsError)
+                        {
+                            throw new Exception("Error in discovery document", discoveryResponse.Exception);
+                        }
+
+                        var tokenResponse = await httpClient.RequestTokenAsync(new TokenRequest
+                        {
+                            Address = discoveryResponse.TokenEndpoint,
+                            GrantType = OidcConstants.GrantTypes.RefreshToken,
+                            ClientId = keycloakOptions.ClientId,
+                            ClientSecret = keycloakOptions.ClientSecret,
+                            Parameters =
+                            {
+                                { OidcConstants.TokenRequest.RefreshToken, context.ProtocolMessage.RefreshToken }
+                            }
+                        });
+
+                        if (tokenResponse.IsError)
+                        {
+                            throw new Exception("Error refreshing token", tokenResponse.Exception);
+                        }
+
+                        if (tokenResponse is { AccessToken: not null, RefreshToken: not null })
+                            context.HandleCodeRedemption(tokenResponse.AccessToken, tokenResponse.RefreshToken);
+                    }
+                };
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = keycloakOptions.Authority;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = keycloakOptions.ValidateIssuer,
+                    ValidIssuer = keycloakOptions.ValidIssuer,
+                    ValidateAudience = keycloakOptions.ValidAudiences != null,
+                    ValidAudiences = keycloakOptions.ValidAudiences,
+                    ValidateLifetime = keycloakOptions.ValidateLifetime
+                };
+            });
+
+        return services;
+    }
+}
